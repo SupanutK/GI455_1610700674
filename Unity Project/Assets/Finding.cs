8fb4f2689eb181657b8e630089cedfad1e56f18f
unknown <zoleheartzzh@gmail.com>
2021-01-27 14:45:53 +0700

    Send Assignment Week1

---
+++ Unity Project/Assets/Finding.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finding : MonoBehaviour
+{
+
+    public string showname;
+    public InputField input;
+    public Text showtext;
+  
+ 
+    public void Findlist()
+    {
+        showname = input.text;
+        if (showname=="ABC" || showname=="DEF" || showname == "CBA" || showname == "FED" || showname == "gg")
+        {
+            showtext.text = "[ "+"<color=green>" + showname+ "</color>" + " ] is found";
+        }
+        else
+        {
+            showtext.text = "[ " + "<color=red>" + showname + "</color>" + " ] is not found";
+        }
+    }
+
+   
+}
